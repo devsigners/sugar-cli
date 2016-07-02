d281f0d9295de9c3cad3dde1c40fd6bdea49e058
creeperyang <yangcreeper@hotmail.com>
2016-07-02 13:54:13 +0800

    refactor inline helper css/js/img, support embed option

--- server/template/builtinFn.js
+++ server/template/builtinFn.js
@@ -11,83 +11,79 @@ const {
     extname
 } = require('path')
 const {
+    readFileSync
+} = require('fs')
+const {
     isEmpty,
     isFunction,
     SafeString
 } = require('sugar-template/lib/utils')
 
-const httpResRe = /(https?:)\/\/.test(url)/i
+const httpResRe = /^(https?:)\/\//i
+const resolveUrl = (url, options, realUrl) => {
+    const isRelative = !isAbsolute(url)
+    let src = url
+    if (isRelative) {
+        let base = options.hash.base || options.$$base
+        if (base) {
+            if (extname(base)) base = join(base, '..')
+            url = join(base, url)
+        }
+        // if just want real file path
+        if (realUrl) return url
+
+        if (options.hash.forceAbsolute && options.$$configRoot) {
+            // Absolute to config.root -- static serve root
+            src = resolve(sep, url.slice(options.$$configRoot.length))
+        } else {
+            // Relative to page url, not partial url
+            src = relative(join(options.$$page, '..'), url)
+        }
+    }
+    return src
+}
 
 module.exports = function(instance) {
     instance.registerHelper('js', function(url, options) {
-        let src = url
-        const isRelative = !httpResRe.test(url) && !isAbsolute(url)
-        if (isRelative) {
-            let base = options.hash.base || options.$$base
-            if (base) {
-                if (extname(base)) base = join(base, '..')
-                url = join(base, url)
-            }
-        }
+        if (httpResRe.test(url)) return new SafeString(`<script src="${url}"></script>`)
 
-        // Finally, we should relative to page url, not partial url
-        if (isRelative) {
-            if (options.hash.forceAbsolute && options.$$configRoot) {
-                src = resolve(sep, url.slice(options.$$configRoot.length))
-            } else {
-                src = relative(join(options.$$page, '..'), url)
-            }
+        let src = resolveUrl(url, options, options.hash.embed)
+        if (options.hash.embed) {
+            let content = ''
+            try {
+                content = readFileSync(src, { encoding: 'utf8' })
+            } catch (e) {}
+            return new SafeString(`<script>${content}</script>`)
         }
-
         return new SafeString(`<script src="${src}"></script>`)
     })
     instance.registerHelper('css', function(url, options) {
-        let src = url
-        const isRelative = !httpResRe.test(url) && !isAbsolute(url)
-        if (isRelative) {
-            let base = options.hash.base || options.$$base
-            if (base) {
-                if (extname(base)) base = join(base, '..')
-                url = join(base, url)
-            }
-        }
+        if (httpResRe.test(url)) return new SafeString(`<link rel="stylesheet" href="${url}">`)
+
+        let src = resolveUrl(url, options, options.hash.embed)
 
         if (options.hash.embed) {
-            return new SafeString(`<style>${'lala'}</style>`)
+            let content = ''
+            try {
+                content = readFileSync(src, { encoding: 'utf8' })
+            } catch (e) {}
+            return new SafeString(`<style>${content}</style>`)
         }
-        // Finally, we should relative to page url, not partial url
-        if (isRelative) {
-            if (options.hash.forceAbsolute && options.$$configRoot) {
-                src = resolve(sep, url.slice(options.$$configRoot.length))
-            } else {
-                src = relative(join(options.$$page, '..'), url)
-            }
-        }
-
         return new SafeString(`<link rel="stylesheet" href="${src}">`)
     })
     instance.registerHelper('img', function(url, options) {
         const alt = options.hash.alt || ''
-        let src = url
-        const isRelative = !httpResRe.test(url) && !isAbsolute(url)
-        if (isRelative) {
-            let base = options.hash.base || options.$$base
-            if (base) {
-                if (extname(base)) base = join(base, '..')
-                url = join(base, url)
-            }
-        }
+        if (httpResRe.test(url)) return new SafeString(`<img src="${url}" alt="${alt}"/>`)
+
+        let src = resolveUrl(url, options, options.hash.embed)
 
         if (options.hash.embed) {
-            return new SafeString(`<img src="base64" alt="${alt}"/>`)
-        }
-        // Finally, we should relative to page url, not partial url
-        if (isRelative) {
-            if (options.hash.forceAbsolute && options.$$configRoot) {
-                src = resolve(sep, url.slice(options.$$configRoot.length))
-            } else {
-                src = relative(join(options.$$page, '..'), url)
-            }
+            let content = ''
+            try {
+                content = readFileSync(src, { encoding: 'base64' })
+                content = `data:image/${extname(src).slice(1)};base64,` + content
+            } catch (e) {}
+            return new SafeString(`<img src="${content}" alt="${alt}"/>`)
         }
 
         return new SafeString(`<img src="${src}" alt="${alt}"/>`)
