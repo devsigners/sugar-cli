3a2991d2f24a8855b922c75487c99d19705a42b2
creeperyang <yangcreeper@hotmail.com>
2017-01-17 19:20:31 +0800

    refactor: rewrite sugar build

--- bin/logger.js
+++ bin/logger.js
@@ -1,12 +1,39 @@
-const colors = require('colors')
+const colors = require('colors/safe')
+const { Logger } = require('../helper/logger')
 
-function colored(text, color = 'black') {
-    return colors[color](text)
-}
-
-exports = module.exports = (name) => {
-    const prefix = colored(name, 'green')
-    return (text, color = 'gray') => {
-        console.log(`${prefix} ${colored(text, color)}`)
+const logger = new Logger({
+    showTime: false,
+    alwaysCheckLevel: false,
+    level: 0,
+    theme: {
+        log: 'gray'
     }
+})
+
+// custom logger
+logger._log = function _log (message, indent = true, level = 'log') {
+    // Check if we should output
+    const levelNum = this.levels[level]
+    if (levelNum < this.level) return
+
+    if (this.alwaysCheckLevel) {
+        const _level = +process.env.LOGLEVEL || 0
+        if (levelNum < _level) return
+    }
+
+    if (typeof message !== 'string') {
+        return console.log()
+    }
+    console[level](
+        (indent ? '  ' : '') + colors[level](message)
+    )
 }
+
+logger.zLog = function zLog (message, indent = true, level, bold) {
+    message = colors[level](message)
+    console[level](
+        (indent ? '  ' : '') + (bold ? colors.bold(message) : message)
+    )
+}
+
+module.exports = logger
