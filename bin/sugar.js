d0a4b01373c800a46185a9b77bf307af630c18ad
creeperyang <yangcreeper@hotmail.com>
2017-01-18 21:30:03 +0800

    refactor: rewrite sugar cmd, rm sugar project & sugar component

--- bin/sugar.js
+++ bin/sugar.js
@@ -1,39 +1,33 @@
 #!/usr/bin/env node
 
-const colors = require('colors') // eslint-disable-line
+const colors = require('colors/safe')
 const pkg = require('../package.json')
 const program = require('./command')
-
-const exitCli = (code = 0) => {
-    process.exit(code)
-}
 
 program.version(pkg.version)
 
 program
-    .command('init <path>', 'Init sugar develop environment')
-    .command('component <name>', 'Create, delete, list component')
-    .command('project <name>', 'Create, delete, list project')
-    .command('start <config>', 'Run develop server')
-    .command('static', 'Run a pure static server')
-    .command('build <config>', 'Build project')
+    .command('init [dir]', 'init sugar develop environment')
+    .command('start <configFileUrl>', 'run develop server')
+    .command('static [dir]', 'serve static files')
+    .command('build [configFileUrl]', 'transform templates to static htmls')
 
 program.on('--help', () => {
-    console.log('  Examples:'.green)
+    console.log(colors.green('  Examples:'))
     console.log('')
-    console.log('    $ sugar start sugar.config.js --watch'.grey)
-    console.log('    $ sugar init uiDir'.grey)
-    console.log('    $ sugar static . --port 3333'.grey)
+    console.log(colors.gray('    $ sugar start sugar.config.js --watch'))
+    console.log(colors.gray('    $ sugar init mydir --autorun'))
+    console.log(colors.gray('    $ sugar static . --port 3333'))
     console.log('')
     console.log('')
-    console.log(`★★★★★  ${
+    console.log(colors.green(`  ★★★★★  ${
         typeof pkg.author === 'string' ? pkg.author : pkg.author.name
-    } ${pkg.name}@v${pkg.version}  ★★★★★`.green)
+    } ${pkg.name}@v${pkg.version}  ★★★★★`))
 })
 
 program.parse(process.argv)
 
 if (program.rawArgs.length === 2) {
-    console.log(`★★★★★  ${pkg.name}@v${pkg.version}  ★★★★★`.green)
-    exitCli()
+    console.log(colors.green(`  ★★★★★  ${pkg.name}@v${pkg.version}  ★★★★★`))
+    process.exit(0)
 }
