94b90b28d311db5f3a192880838fde8b419d3811
creeperyang <yangcreeper@hotmail.com>
2017-01-19 17:36:29 +0800

    refactor: update static server

--- server/static/serve.js
+++ server/static/serve.js
@@ -2,10 +2,9 @@ const Koa = require('koa')
 const logger = require('koa-logger')
 const serve = require('./index')
 
-const app = new Koa()
-app.use(logger())
-
 module.exports = function (host, port, root, cb) {
+    const app = new Koa()
+    app.use(logger())
     app.use(serve(root))
     app.listen(port, host, err => {
         cb && cb(err)
